831176cf1518a51d2a3623db5a730052769d634e
julia sohkanen <73934921+julia-sand@users.noreply.github.com>
2026-08-12 14:13:36 +0300

    day 2 afternoon

---
+++ openmp/exercises/03-axpy-data/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hello
+#SBATCH --account=project_2019754
+#SBATCH --partition=gpumedium
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:gh200:1
+#SBATCH --time=00:02:00
+#SBATCH --out=axpy.out
+
+# Enable OpenMP offload debugging by uncommenting the line below
+export NVCOMPILER_ACC_NOTIFY=3
+export OMP_NUM_TEAMS=16
+
+# Run the program
+srun ./axpy.o
---
+++ openmp/exercises/06-heat-reduction/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=hello
+#SBATCH --account=project_2019754
+#SBATCH --partition=gpumedium
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:gh200:1
+#SBATCH --time=00:02:00
+#SBATCH --out=heat1.out
+
+# Enable OpenMP offload debugging by uncommenting the line below
+export NVCOMPILER_ACC_NOTIFY=3
+
+# Run the program
+srun nsys profile ./heat_reduce.o
